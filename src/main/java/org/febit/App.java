5f91bbb31e0859363ba3d17e2f770b7c36bc1b3b
zqq90 <zqq_90@163.com>
2016-07-23 20:08:37 +0800

    Add: App

---
+++ src/main/java/org/febit/App.java
@@ -0,0 +1,137 @@
+// Copyright (c) 2013-present, febit.org. All Rights Reserved.
+package org.febit;
+
+import java.util.ArrayList;
+import java.util.List;
+import jodd.util.ClassLoaderUtil;
+import org.febit.lang.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.febit.service.Services;
+import org.febit.util.Petite;
+import org.febit.util.PriorityUtil;
+import org.febit.util.Props;
+import org.febit.util.PropsUtil;
+
+/**
+ *
+ * @author zqq90
+ */
+public class App implements Singleton {
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    protected Props _props = new Props();
+    protected Listener[] _listeners;
+    protected Petite _petite;
+
+    //settings
+    protected String name = "App";
+    protected Object[] beans;
+
+    public void start(String propsFiles) {
+        loadProps(propsFiles);
+        initPetite();
+        try {
+            startListeners();
+            LOG.info("> App start: " + name);
+        } catch (Exception ex) {
+            LOG.error("> Failed start: " + name, ex);
+            stop();
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void stop() {
+        stopListeners();
+        LOG.info("> App stopped: " + name);
+    }
+
+    protected void loadProps(String propsFiles) {
+        if (propsFiles == null) {
+            return;
+        }
+        if (propsFiles.indexOf('*') >= 0) {
+            PropsUtil.scanClasspath(_props, propsFiles);
+        } else {
+            PropsUtil.loadFromClasspath(_props, propsFiles);
+        }
+    }
+
+    protected void initPetite() {
+        LOG.info("Loaded props: {}", _props.getModulesString());
+        this._petite = new Petite();
+        Services.setPetite(_petite);
+        this._petite.setProps(_props, null);
+        this._petite.addGlobalBean(this);
+        this._petite.init();
+        this._petite.regist("app", this);
+    }
+
+    protected void startListeners() {
+        stopListeners();
+        if (this.beans == null) {
+            return;
+        }
+        final List<Listener> listeners = new ArrayList<>(this.beans.length);
+        for (Object bean : this.beans) {
+            if (bean instanceof Listener) {
+                listeners.add((Listener) bean);
+            }
+        }
+        if (listeners.isEmpty()) {
+            return;
+        }
+        this._listeners = listeners.toArray(new Listener[listeners.size()]);
+        PriorityUtil.desc(this._listeners);
+        for (Listener listener : this._listeners) {
+            LOG.info(">> starting listener:" + listener.getClass());
+            listener.start();
+        }
+    }
+
+    protected void stopListeners() {
+        Listener[] listeners = this._listeners;
+        if (listeners == null) {
+            return;
+        }
+        for (int i = listeners.length - 1; i >= 0; i--) {
+            Listener listener = listeners[i];
+            LOG.info(">> stoping listener:" + listener.getClass());
+            listener.stop();
+        }
+        this._listeners = null;
+    }
+
+    public Object createBean(String type) throws ClassNotFoundException,
+            InstantiationException, IllegalAccessException {
+        return createBean(ClassLoaderUtil.getDefaultClassLoader().loadClass(type));
+    }
+
+    public Object createBean(Class type)
+            throws InstantiationException, IllegalAccessException {
+        final Object bean = type.newInstance();
+        this._petite.inject(bean);
+        return bean;
+    }
+
+    public Petite getPetite() {
+        return this._petite;
+    }
+
+    public void injectBean(final Object bean) {
+        this._petite.inject(bean);
+    }
+
+    public void addBean(final Object bean) {
+        this._petite.regist(bean);
+    }
+
+    public Object getBean(String name) {
+        return this._petite.get(name);
+    }
+
+    public <T> T getBean(Class<T> type) {
+        return (T) this._petite.get(type);
+    }
+}
---
+++ src/main/java/org/febit/Listener.java
@@ -0,0 +1,13 @@
+// Copyright (c) 2013-present, febit.org. All Rights Reserved.
+package org.febit;
+
+/**
+ *
+ * @author zqq90
+ */
+public interface Listener {
+
+    void start();
+
+    void stop();
+}
