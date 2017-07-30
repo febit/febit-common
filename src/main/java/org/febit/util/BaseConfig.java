3f8f93d6ce2166c886aa27f15fbd5fcfd4b45edc
zqq90 <zqq_90@163.com>
2017-07-30 21:56:08 +0800

    update BaseConfig

--- src/main/java/org/febit/util/BaseConfig.java
+++ src/main/java/org/febit/util/BaseConfig.java
@@ -15,31 +15,37 @@
  */
 package org.febit.util;
 
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import org.febit.convert.Convert;
+import org.febit.lang.Singleton;
 
 /**
  *
  * @author zqq90
+ * @param <T>
  */
-public class BaseConfig {
+public class BaseConfig<T extends BaseConfig> implements Singleton, Serializable {
 
-    protected final Map<String, String> datas;
+    private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(BaseConfig.class);
+    protected final Map<String, String> data;
+
+    protected BaseConfig() {
+        this(new HashMap<String, String>());
+    }
 
     protected BaseConfig(Map<String, String> datas) {
-        this.datas = datas;
+        this.data = datas;
     }
 
     public boolean checkRequire(String key) {
-        return datas.containsKey(key);
+        return data.containsKey(key);
     }
 
-    /**
-     * 返回
-     *
-     * @param keys
-     * @return
-     */
+    @SuppressWarnings("unchecked")
     public String[] checkRequire(String[] keys) {
         if (keys == null) {
             return null;
@@ -57,7 +63,7 @@ public class BaseConfig {
     }
 
     public String get(String key) {
-        return datas.get(key);
+        return data.get(key);
     }
 
     public String getOrElse(String key, String defaultValue) {
@@ -66,5 +72,80 @@ public class BaseConfig {
             return defaultValue;
         }
         return ret;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T putAll(Props props) {
+        this.data.putAll(props.export());
+        return (T) this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T putAll(Map<String, String> datas) {
+        this.data.putAll(datas);
+        return (T) this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T put(String key, String value) {
+        this.data.put(key, value);
+        return (T) this;
+    }
+
+    public String[] getStringArray(String key) {
+        return StringUtil.toArray(get(key));
+    }
+
+    public int getInt(String key) {
+        return Convert.toInt(get(key));
+    }
+
+    public boolean getBool(String key) {
+        return Convert.toBool(get(key));
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map<String, String> export() {
+        Map<String, String> result = CollectionUtil.createMap(this.data.size());
+        exportTo(result);
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void exportTo(final Map target) {
+        target.putAll(this.data);
+    }
+
+    public Map<String, String> extract(String prefix) {
+        Map<String, String> map = new HashMap<>();
+        if (StringUtil.isEmpty(prefix)) {
+            exportTo(map);
+            return map;
+        }
+        int prefixLength = prefix.length();
+        for (Map.Entry<String, String> entry : this.data.entrySet()) {
+            String key = entry.getKey();
+            if (key == null
+                    || !key.startsWith(prefix)) {
+                continue;
+            }
+            map.put(key.substring(prefixLength), entry.getValue());
+        }
+        return map;
+    }
+
+    public Petite buildPetite() {
+        return Petite.builder()
+                .addGlobalBean(this)
+                .addProps(export())
+                .build();
+    }
+
+    public Set<String> keySet() {
+        return this.data.keySet();
+    }
+
+    public int size() {
+        return this.data.size();
     }
 }
