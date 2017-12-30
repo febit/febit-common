763e057fc79a4088ab1fdcaf9507b0488e59c29b
zqq90 <zqq_90@163.com>
2017-12-30 12:38:12 +0800

    update CountryCodeUtil

--- src/main/java/org/febit/util/CountryCodeUtil.java
+++ src/main/java/org/febit/util/CountryCodeUtil.java
@@ -23,36 +23,34 @@ import java.util.Map;
  */
 public class CountryCodeUtil {
 
-    private static final Map<String, String> CODE_TO_COUNTRY_MAP;
+    private static class DictHolder {
 
-    private static final Map<String, String> COUNTRY_TO_CODE_MAP;
+        static final Map<String, String> CODE_TO_COUNTRY_MAP;
+        static final Map<String, String> COUNTRY_TO_CODE_MAP;
 
-    static {
-        CODE_TO_COUNTRY_MAP = Props.shadowLoader()
-                .load("classpath:org/febit/util/country-code.dict")
-                .get()
-                .export();
+        static {
+            CODE_TO_COUNTRY_MAP = Props.shadowLoader()
+                    .load("classpath:org/febit/util/country-code.dict")
+                    .get()
+                    .export();
 
-        Map<String, String> reverse = CollectionUtil.createMap(CODE_TO_COUNTRY_MAP.size());
-        for (Map.Entry<String, String> entry : CODE_TO_COUNTRY_MAP.entrySet()) {
-            reverse.put(entry.getValue().trim().toUpperCase(), entry.getKey());
+            Map<String, String> reverse = CollectionUtil.createMap(CODE_TO_COUNTRY_MAP.size());
+            CODE_TO_COUNTRY_MAP.forEach((key, value) -> reverse.put(value.trim().toUpperCase(), key));
+            COUNTRY_TO_CODE_MAP = reverse;
         }
-        COUNTRY_TO_CODE_MAP = reverse;
     }
 
     public static String getCountryZhName(String code) {
         if (code == null) {
             return null;
         }
-        code = code.trim().toUpperCase();
-        return CODE_TO_COUNTRY_MAP.get(code);
+        return DictHolder.CODE_TO_COUNTRY_MAP.get(code.trim().toUpperCase());
     }
 
     public static String nameToCode(String name) {
         if (name == null) {
             return null;
         }
-        name = name.trim().toUpperCase();
-        return COUNTRY_TO_CODE_MAP.get(name);
+        return DictHolder.COUNTRY_TO_CODE_MAP.get(name.trim().toUpperCase());
     }
 }
