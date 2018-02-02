069d0ade455455517a5f053ba12c49708252d371
zqq90 <zqq_90@163.com>
2018-02-02 12:03:53 +0800

    Add TerConsumer

---
+++ src/main/java/org/febit/lang/TerConsumer.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 febit.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.febit.lang;
+
+import java.util.Objects;
+
+/**
+ * Represents an operation that accepts tree input arguments and returns no result.
+ *
+ * @param <T> the type of the first argument to the operation
+ * @param <U> the type of the second argument to the operation
+ * @param <V> the type of the third argument to the operation
+ *
+ * @see java.util.function.Consumer
+ * @author zqq90
+ */
+@FunctionalInterface
+public interface TerConsumer<T, U, V> {
+
+    /**
+     * Performs this operation on the given arguments.
+     *
+     * @param t the first input argument
+     * @param u the second input argument
+     * @param v the third input argument
+     */
+    void accept(T t, U u, V v);
+
+    /**
+     * Returns a composed {@code TerConsumer} that performs, in sequence, this operation followed by the {@code after}
+     * operation.
+     *
+     * @param after the operation to perform after this operation
+     * @return a composed {@code TerConsumer} that performs in sequence this operation followed by the {@code after}
+     * operation
+     * @throws NullPointerException if {@code after} is null
+     */
+    default TerConsumer<T, U, V> andThen(TerConsumer<? super T, ? super U, ? super V> after) {
+        Objects.requireNonNull(after);
+        return (t, u, v) -> {
+            accept(t, u, v);
+            after.accept(t, u, v);
+        };
+    }
+}
