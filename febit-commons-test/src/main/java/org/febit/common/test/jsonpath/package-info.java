a5ef8958fe906987234a71ecf766df2cde40e0e2
zqq90 <zqq_90@163.com>
2023-09-18 00:06:44 +0800

    refactor: rename test module

--- febit-commons-test/src/main/java/org/febit/common/test/jsonpath/package-info.java
+++ febit-commons-test/src/main/java/org/febit/common/test/jsonpath/package-info.java
@@ -13,4 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.febit.test.common;
+@NonNullApi
+package org.febit.common.test.jsonpath;
+
+import org.febit.lang.annotation.NonNullApi;
--- febit-commons-test/src/main/java/org/febit/common/test/package-info.java
+++ febit-commons-test/src/main/java/org/febit/common/test/package-info.java
@@ -13,7 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@NonNullApi
-package org.febit.test.common.jsonpath;
-
-import org.febit.lang.annotation.NonNullApi;
+package org.febit.common.test;
