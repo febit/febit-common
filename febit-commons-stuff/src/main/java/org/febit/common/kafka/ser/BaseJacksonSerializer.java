864fc945638813caf41f1b73abd2713f1c882ad7
zqq90 <zqq_90@163.com>
2023-09-24 20:45:08 +0800

    feat(kafka): add serializers

---
+++ febit-commons-stuff/src/main/java/org/febit/common/kafka/ser/BaseJacksonSerializer.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2013-present febit.org (support@febit.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.febit.common.kafka.ser;
+
+import jakarta.annotation.Nullable;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.common.serialization.Serializer;
+import org.febit.lang.util.JacksonWrapper;
+
+import java.nio.charset.StandardCharsets;
+
+@RequiredArgsConstructor
+public abstract class BaseJacksonSerializer<T> implements Serializer<T> {
+
+    private final JacksonWrapper jackson;
+
+    @Override
+    public byte[] serialize(String topic, @Nullable T data) {
+        var text = jackson.toString(data);
+        return text.getBytes(StandardCharsets.UTF_8);
+    }
+}
