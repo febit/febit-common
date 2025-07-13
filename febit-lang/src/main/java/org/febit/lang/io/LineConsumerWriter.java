c4a9747638826b97f45ed03e10d8180f05ef491f
zqq90 <zqq_90@163.com>
2025-07-13 22:47:17 +0800

    chore: license header

--- febit-lang/src/main/java/org/febit/lang/io/LineConsumerWriter.java
+++ febit-lang/src/main/java/org/febit/lang/io/LineConsumerWriter.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2013-present febit.org (support@febit.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.febit.lang.io;
 
 import lombok.RequiredArgsConstructor;
 
 import java.io.Writer;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
 @RequiredArgsConstructor(staticName = "create")
@@ -12,6 +28,7 @@ public class LineConsumerWriter extends Writer {
     private static final char CR = '\r';
 
     private final StringBuilder remaining = new StringBuilder();
+    private final AtomicBoolean closed = new AtomicBoolean(false);
     private final Consumer<String> consumer;
 
     @Override
@@ -98,6 +115,10 @@ public class LineConsumerWriter extends Writer {
         remaining.setLength(0);
     }
 
+    public boolean closed() {
+        return closed.get();
+    }
+
     @Override
     public void flush() {
         // No operation
@@ -105,6 +126,7 @@ public class LineConsumerWriter extends Writer {
 
     @Override
     public void close() {
+        closed.set(true);
         flush();
         popRemaining();
     }
