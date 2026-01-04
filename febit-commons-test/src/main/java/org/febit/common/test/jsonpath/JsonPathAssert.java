c22248359d260a7b1ec190785f14748bbc64b492
zqq90 <zqq_90@163.com>
2026-01-04 15:00:41 +0800

    feat(test): enhance JsonPathAssert with generics and nullable

--- febit-commons-test/src/main/java/org/febit/common/test/jsonpath/JsonPathAssert.java
+++ febit-commons-test/src/main/java/org/febit/common/test/jsonpath/JsonPathAssert.java
@@ -16,10 +16,10 @@
 package org.febit.common.test.jsonpath;
 
 import com.jayway.jsonpath.Configuration;
-import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.EvaluationListener;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.cache.CacheProvider;
 import com.jayway.jsonpath.spi.json.JsonProvider;
 import com.jayway.jsonpath.spi.mapper.MappingProvider;
 import jakarta.annotation.Nullable;
@@ -44,22 +44,46 @@ import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
 
-public class JsonPathAssert extends AbstractAssert<JsonPathAssert, Object> {
+public class JsonPathAssert<T> extends AbstractAssert<JsonPathAssert<T>, T> {
 
-    private final DocumentContext context;
-    final Conditions conditions = Conditions.instance();
+    private final Conditions conditions = Conditions.instance();
+    private final Configuration conf;
 
-    protected JsonPathAssert(DocumentContext context) {
-        super(context.json(), JsonPathAssert.class);
-        this.context = context;
+    protected JsonPathAssert(T root, Configuration conf) {
+        super(root, JsonPathAssert.class);
+        this.conf = conf;
+    }
+
+    private JsonPath compileIfAbsent(String pattern) {
+        var cache = CacheProvider.getCache();
+        var path = cache.get(pattern);
+        if (path == null) {
+            path = JsonPath.compile(pattern);
+            cache.put(pattern, path);
+        }
+        return path;
+    }
+
+    public static <T> JsonPathAssert.Builder<T> builder() {
+        return new JsonPathAssert.Builder<>();
+    }
+
+    public static <T> JsonPathAssert.Builder<T> builder(String json) {
+        return new JsonPathAssert.Builder<T>()
+                .json(json);
+    }
+
+    public static <T> JsonPathAssert.Builder<T> builder(T parsed) {
+        return new JsonPathAssert.Builder<T>()
+                .parsed(parsed);
     }
 
     @lombok.Builder(
             builderClassName = "Builder"
     )
-    private static JsonPathAssert build0(
+    private static <T> JsonPathAssert<T> create(
             @Nullable String json,
-            @Nullable Object parsed,
+            @Nullable T parsed,
             @Nullable final JsonProvider jsonProvider,
             @Nullable final MappingProvider mappingProvider,
             @Singular final Set<Option> options,
@@ -67,202 +91,217 @@ public class JsonPathAssert extends AbstractAssert<JsonPathAssert, Object> {
     ) {
         var conf = Configuration.builder()
                 .jsonProvider(
-                        jsonProvider != null ? jsonProvider : conf().jsonProvider()
+                        jsonProvider != null ? jsonProvider : defaultConf().jsonProvider()
                 )
                 .mappingProvider(
-                        mappingProvider != null ? mappingProvider : conf().mappingProvider()
+                        mappingProvider != null ? mappingProvider : defaultConf().mappingProvider()
                 )
                 .options(options)
+                .options(Option.SUPPRESS_EXCEPTIONS)
                 .evaluationListener(evaluationListeners)
                 .build();
-        return parsed != null
-                ? assertJsonPath(parsed, conf)
-                : assertJsonPath(json, conf);
-    }
 
-    public static JsonPathAssert of(DocumentContext context) {
-        return new JsonPathAssert(context);
-    }
-
-    public static JsonPathAssert assertJsonPath(@Nullable Object parsed) {
-        return assertJsonPath(parsed, conf());
-    }
-
-    public static JsonPathAssert assertJsonPath(@Nullable String json) {
-        return assertJsonPath(json, conf());
-    }
-
-    public static JsonPathAssert assertJsonPath(@Nullable Object parsed, Configuration conf) {
-        return of(
-                JsonPath.parse(parsed, conf)
-        );
-    }
-
-    public static JsonPathAssert assertJsonPath(@Nullable String json, Configuration conf) {
-        if (json == null) {
-            return assertJsonPath((Object) null, conf);
+        if (parsed == null && json != null) {
+            return assertJsonPath(json, conf);
         }
-        return of(
-                JsonPath.parse(json, conf)
-        );
+        return assertJsonPath(parsed, conf);
     }
 
-    public static Configuration conf() {
+    public static <T> JsonPathAssert<T> of(T obj, Configuration conf) {
+        return new JsonPathAssert<>(obj, conf);
+    }
+
+    public static <T> JsonPathAssert<T> assertJsonPath(@Nullable T parsed) {
+        return assertJsonPath(parsed, defaultConf());
+    }
+
+    public static <T> JsonPathAssert<T> assertJsonPath(@Nullable String json) {
+        return assertJsonPath(json, defaultConf());
+    }
+
+    public static <T> JsonPathAssert<T> assertJsonPath(@Nullable T parsed, Configuration conf) {
+        return of(parsed, conf);
+    }
+
+    public static <T> JsonPathAssert<T> assertJsonPath(@Nullable String json, Configuration conf) {
+        if (json == null) {
+            return assertJsonPath((T) null, conf);
+        }
+        @SuppressWarnings("unchecked")
+        var parsed = (T) conf.jsonProvider().parse(json);
+        return of(parsed, conf);
+    }
+
+    public static Configuration defaultConf() {
         return ConfLazyHolder.CONF;
     }
 
-    public <T> T read(String path) {
-        return context.read(path);
+    @Nullable
+    @SuppressWarnings("unchecked")
+    public <R> R read(String pattern) {
+        var path = compileIfAbsent(pattern);
+        var root = root();
+        if (root == null) {
+            return path.isDefinite() ? null : (R) List.of();
+        }
+        return path.read(root, conf);
     }
 
-    public <T> T read(String path, Class<T> type) {
-        return context.read(path, type);
+    @Nullable
+    public <R> R read(String pattern, Class<R> type) {
+        var path = compileIfAbsent(pattern);
+        var result = path.read(root(), conf);
+        return conf.mappingProvider().map(result, type, conf);
     }
 
-    public <T> T root() {
-        return context.json();
+    @Nullable
+    public T root() {
+        return actual;
     }
 
-    public JsonPathAssert isEqualTo(String path, Object expected) {
+    public JsonPathAssert<T> isEqualTo(String path, Object expected) {
         objects.assertEqual(info, read(path), expected);
         return myself;
     }
 
-    public JsonPathAssert isNotEqualTo(String path, Object other) {
+    public JsonPathAssert<T> isNotEqualTo(String path, Object other) {
         objects.assertNotEqual(info, read(path), other);
         return myself;
     }
 
-    public JsonPathAssert isNull(String path) {
+    public JsonPathAssert<T> isNull(String path) {
         objects.assertNull(info, read(path));
         return myself;
     }
 
-    public JsonPathAssert isNotNull(String path) {
+    public JsonPathAssert<T> isNotNull(String path) {
         objects.assertNotNull(info, read(path));
         return myself;
     }
 
-    public JsonPathAssert isSameAs(String path, Object expected) {
+    public JsonPathAssert<T> isSameAs(String path, Object expected) {
         objects.assertSame(info, read(path), expected);
         return myself;
     }
 
-    public JsonPathAssert isNotSameAs(String path, Object other) {
+    public JsonPathAssert<T> isNotSameAs(String path, Object other) {
         objects.assertNotSame(info, read(path), other);
         return myself;
     }
 
-    public JsonPathAssert isInstanceOf(String path, Class<?> type) {
+    public JsonPathAssert<T> isInstanceOf(String path, Class<?> type) {
         objects.assertIsInstanceOf(info, read(path), type);
         return myself;
     }
 
-    public JsonPathAssert isInstanceOfAny(String path, Class<?>... types) {
+    public JsonPathAssert<T> isInstanceOfAny(String path, Class<?>... types) {
         objects.assertIsInstanceOfAny(info, read(path), types);
         return myself;
     }
 
-    public JsonPathAssert isNotInstanceOf(String path, Class<?> type) {
+    public JsonPathAssert<T> isNotInstanceOf(String path, Class<?> type) {
         objects.assertIsNotInstanceOf(info, read(path), type);
         return myself;
     }
 
-    public JsonPathAssert isNotInstanceOfAny(String path, Class<?>... types) {
+    public JsonPathAssert<T> isNotInstanceOfAny(String path, Class<?>... types) {
         objects.assertIsNotInstanceOfAny(info, read(path), types);
         return myself;
     }
 
-    public JsonPathAssert hasSameClassAs(String path, Object other) {
+    public JsonPathAssert<T> hasSameClassAs(String path, Object other) {
         objects.assertHasSameClassAs(info, read(path), other);
         return myself;
     }
 
-    public JsonPathAssert doesNotHaveSameClassAs(String path, Object other) {
+    public JsonPathAssert<T> doesNotHaveSameClassAs(String path, Object other) {
         objects.assertDoesNotHaveSameClassAs(info, read(path), other);
         return myself;
     }
 
-    public JsonPathAssert hasToString(String path, String expectedToString) {
+    public JsonPathAssert<T> hasToString(String path, String expectedToString) {
         objects.assertHasToString(info, read(path), expectedToString);
         return myself;
     }
 
-    public JsonPathAssert doesNotHaveToString(String path, String otherToString) {
+    public JsonPathAssert<T> doesNotHaveToString(String path, String otherToString) {
         objects.assertDoesNotHaveToString(info, read(path), otherToString);
         return myself;
     }
 
-    public JsonPathAssert isExactlyInstanceOf(String path, Class<?> type) {
+    public JsonPathAssert<T> isExactlyInstanceOf(String path, Class<?> type) {
         objects.assertIsExactlyInstanceOf(info, read(path), type);
         return myself;
     }
 
-    public JsonPathAssert isNotExactlyInstanceOf(String path, Class<?> type) {
+    public JsonPathAssert<T> isNotExactlyInstanceOf(String path, Class<?> type) {
         objects.assertIsNotExactlyInstanceOf(info, read(path), type);
         return myself;
     }
 
-    public JsonPathAssert hasSameHashCodeAs(String path, Object other) {
+    public JsonPathAssert<T> hasSameHashCodeAs(String path, Object other) {
         objects.assertHasSameHashCodeAs(info, read(path), other);
         return myself;
     }
 
-    public JsonPathAssert doesNotHaveSameHashCodeAs(String path, Object other) {
+    public JsonPathAssert<T> doesNotHaveSameHashCodeAs(String path, Object other) {
         objects.assertDoesNotHaveSameHashCodeAs(info, read(path), other);
         return myself;
     }
 
-    public JsonPathAssert is(String path, Condition<Object> condition) {
+    public JsonPathAssert<T> is(String path, Condition<Object> condition) {
         conditions.assertIs(info, read(path), condition);
         return myself;
     }
 
-    public JsonPathAssert isNot(String path, Condition<Object> condition) {
+    public JsonPathAssert<T> isNot(String path, Condition<Object> condition) {
         conditions.assertIsNot(info, read(path), condition);
         return myself;
     }
 
-    public JsonPathAssert has(String path, Condition<Object> condition) {
+    public JsonPathAssert<T> has(String path, Condition<Object> condition) {
         conditions.assertHas(info, read(path), condition);
         return myself;
     }
 
-    public JsonPathAssert doesNotHave(String path, Condition<Object> condition) {
+    public JsonPathAssert<T> doesNotHave(String path, Condition<Object> condition) {
         conditions.assertDoesNotHave(info, read(path), condition);
         return myself;
     }
 
-    public JsonPathAssert satisfies(String path, Condition<Object> condition) {
+    public JsonPathAssert<T> satisfies(String path, Condition<Object> condition) {
         conditions.assertSatisfies(info, read(path), condition);
         return myself;
     }
 
     @CheckReturnValue
-    public JsonPathAssert dive(String path) {
-        var value = read(path);
-        return assertJsonPath(value, context.configuration());
+    public <R> JsonPathAssert<R> dive(String path) {
+        @SuppressWarnings("unchecked")
+        var value = (R) read(path);
+        return assertJsonPath(value, conf);
     }
 
     @CheckReturnValue
-    public ListAssert<?> asList(String path) {
-        var value = read(path);
+    public <R> ListAssert<R> asList(String path) {
+        @SuppressWarnings("unchecked")
+        var value = (List<R>) read(path);
         objects.assertIsInstanceOf(info, value, List.class);
-        return Assertions.assertThat((List<?>) value);
+        return Assertions.assertThat(value);
     }
 
     @CheckReturnValue
-    public MapAssert<?, ?> asMap(String path) {
-        var value = read(path);
+    public <K, V> MapAssert<K, V> asMap(String path) {
+        @SuppressWarnings("unchecked")
+        var value = (Map<K, V>) read(path);
         objects.assertIsInstanceOf(info, value, Map.class);
-        return Assertions.assertThat((Map<?, ?>) value);
+        return Assertions.assertThat(value);
     }
 
     @CheckReturnValue
     public AbstractStringAssert<?> asString(String path) {
-        var value = read(path);
+        var value = (String) read(path);
         objects.assertIsInstanceOf(info, value, String.class);
-        return Assertions.assertThat((String) value);
+        return Assertions.assertThat(value);
     }
 
     @CheckReturnValue
@@ -271,17 +310,16 @@ public class JsonPathAssert extends AbstractAssert<JsonPathAssert, Object> {
         return Assertions.assertThat(value);
     }
 
-    @SuppressWarnings("unchecked")
     @CheckReturnValue
-    public <T, ASSERT extends AbstractAssert<?, ?>> ASSERT as(
-            String path, Class<T> type,
-            AssertFactory<T, ASSERT> assertFactory
+    public <R, ASSERT extends AbstractAssert<?, ?>> ASSERT as(
+            String path, Class<R> type,
+            AssertFactory<R, ASSERT> assertFactory
     ) {
         requireNonNull(type);
         requireNonNull(assertFactory);
-        var value = read(path);
+        var value = read(path, type);
         objects.assertIsInstanceOf(info, value, type);
-        return assertFactory.createAssert((T) value);
+        return assertFactory.createAssert(value);
     }
 
     private static class ConfLazyHolder {
@@ -293,6 +331,7 @@ public class JsonPathAssert extends AbstractAssert<JsonPathAssert, Object> {
             CONF = Configuration.builder()
                     .jsonProvider(new Jackson3JsonProvider(mapper))
                     .mappingProvider(new Jackson3MappingProvider(mapper))
+                    .options(Option.SUPPRESS_EXCEPTIONS)
                     .build();
         }
     }
--- febit-commons-test/src/test/java/org/febit/common/test/jsonpath/JsonPathAssertTest.java
+++ febit-commons-test/src/test/java/org/febit/common/test/jsonpath/JsonPathAssertTest.java
@@ -16,34 +16,61 @@
 package org.febit.common.test.jsonpath;
 
 import com.jayway.jsonpath.InvalidJsonException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.HamcrestCondition.matching;
 import static org.febit.common.test.jsonpath.JsonPathAssert.assertJsonPath;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
 
 class JsonPathAssertTest {
 
-    @Test
-    void bad() {
-        assertThatThrownBy(() -> JsonPathAssert.builder().build())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("json object can not be null");
+    final JsonPathAssert<Object> root$ = assertJsonPath("""
+            {
+              "name": "febit",
+              "age": 10,
+              "tags": ["java", "json", "rpc"],
+              "meta": {
+                "host": "febit.org"
+              }
+            }
+            """);
 
-        assertThatThrownBy(() -> assertJsonPath("null"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("json can not be null");
+    @Test
+    void nullable() {
+        JsonPathAssert.builder().build()
+                .isNull("$")
+                .isEqualTo("$..missing", List.of())
+                .isEqualTo("$.missing[2,11]", List.of())
+        ;
+        assertJsonPath("null")
+                .isNull("$")
+                .isNull();
+
+        assertJsonPath("{}")
+                .isEqualTo("$", Map.of())
+                .isEqualTo("$..missing", List.of())
+                .isEqualTo("$..missing[2,11]", List.of())
+        ;
     }
 
     @Test
     void invalid() {
         assertThatThrownBy(() -> assertJsonPath("invalid json"))
                 .isInstanceOf(InvalidJsonException.class);
+
+        assertJsonPath("{}")
+                .isNull("$.missing")
+                .isEqualTo("$.missing[2,11]", List.of());
     }
 
     @Test
@@ -63,30 +90,169 @@ class JsonPathAssertTest {
     }
 
     @Test
-    void ok() {
-        var json = """
-                {
-                  "name": "febit",
-                  "age": 10,
-                  "tags": ["java", "json", "rpc"],
-                  "meta": {
-                    "host": "febit.org"
-                  }
-                }
-                """;
-        assertJsonPath(json)
-                .isNotNull()
+    void root$() {
+        var root = root$.root();
+        assertNotNull(root);
+
+        root$.isNotNull()
                 .isInstanceOf(Map.class)
+                .isSameAs("$", root)
+                .isNotNull("$.name")
                 .is("$.name", matching(containsString("bit")))
                 .is("$.age", matching(equalTo(10)))
+                .isInstanceOf("$.tags", List.class)
                 .isEqualTo("$.tags[1]", "json")
                 .isEqualTo("$.meta.host", "febit.org")
-                .isInstanceOf("$.tags", List.class)
-                .dive("$.tags")
-                .isEqualTo("$.[0]", "java")
-        // .isNull("$.meta.missing") // TODO FIX: PathNotFoundException
+                .isNull("$.meta.missing")
+        ;
+    }
+
+    @Test
+    void comparisons() {
+        assertThatThrownBy(() -> root$.isNull("$.name"))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isNull("$.missing"));
+
+        assertThatThrownBy(() -> root$.isNotEqualTo("$.age", 10))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isNotEqualTo("$.age", 11));
+
+        assertDoesNotThrow(() -> root$.isEqualTo("$.tags", List.of("java", "json", "rpc")));
+        assertThatThrownBy(() -> root$.isSameAs("$.tags", List.of("java", "json", "rpc")))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isSameAs("$.tags", Objects.requireNonNull(
+                root$.dive("$.tags").root()
+        )));
+
+        assertThatThrownBy(() -> root$.isNotSameAs("$.meta", Objects.requireNonNull(
+                root$.dive("$.meta").root()
+        ))).isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isNotSameAs("$.meta", Map.of()));
+    }
+
+    @Test
+    void conditions() {
+        assertThatThrownBy(() -> root$.is("$.name", matching(containsString("bits"))))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.is("$.name", matching(containsString("bit"))));
+
+        assertThatThrownBy(() -> root$.isNot("$.age", matching(equalTo(10))))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isNot("$.age", matching(equalTo(11))));
+
+        assertThatThrownBy(() -> root$.has("$.name", matching(containsString("bits"))))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.has("$.name", matching(containsString("bit"))));
+
+        assertThatThrownBy(() -> root$.doesNotHave("$.age", matching(equalTo(10))))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.doesNotHave("$.age", matching(equalTo(11))));
+
+        assertThatThrownBy(() -> root$.satisfies("$.name", matching(containsString("bits"))))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.satisfies("$.name", matching(containsString("bit"))));
+    }
+
+    @Test
+    void others() {
+        assertThatThrownBy(() -> root$.hasToString("$.name", "febit.org"))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.hasToString("$.name", "febit"));
+
+        assertThatThrownBy(() -> root$.doesNotHaveToString("$.age", "10"))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.doesNotHaveToString("$.age", "11"));
+
+        assertThatThrownBy(() -> root$.hasSameHashCodeAs("$.age", 11))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.hasSameHashCodeAs("$.age", 10));
+
+        assertThatThrownBy(() -> root$.doesNotHaveSameHashCodeAs("$.name", "febit"))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.doesNotHaveSameHashCodeAs("$.name", "febit.org"));
+    }
+
+    @Test
+    void instanceOfs() {
+        assertThatThrownBy(() -> root$.isInstanceOf("$.meta", List.class))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isInstanceOf("$.meta", Map.class));
+
+        assertThatThrownBy(() -> root$.isInstanceOfAny("$.name", Integer.class, Boolean.class))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isInstanceOfAny("$.age", Integer.class, Number.class));
+
+        assertThatThrownBy(() -> root$.isNotInstanceOf("$.tags", List.class))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isNotInstanceOf("$.tags", Map.class));
+
+        assertThatThrownBy(() -> root$.isNotInstanceOfAny("$.age", Integer.class, Number.class))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isNotInstanceOfAny("$.name", Number.class, Boolean.class));
+
+        assertThatThrownBy(() -> root$.isExactlyInstanceOf("$.name", Object.class))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isExactlyInstanceOf("$.name", String.class));
+
+        assertThatThrownBy(() -> root$.isNotExactlyInstanceOf("$.age", Integer.class))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.isNotExactlyInstanceOf("$.age", Number.class));
+
+        assertThatThrownBy(() -> root$.hasSameClassAs("$.meta", new HashMap<>()))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.hasSameClassAs("$.meta", new LinkedHashMap<>()));
+
+        assertThatThrownBy(() -> root$.doesNotHaveSameClassAs("$.meta", new LinkedHashMap<>()))
+                .isInstanceOf(AssertionError.class);
+        assertDoesNotThrow(() -> root$.doesNotHaveSameClassAs("$.meta", new HashMap<>()));
+    }
+
+    @Test
+    void meta$() {
+        var meta$ = root$.dive("$.meta");
+        meta$.isNotNull()
+                .isInstanceOf(Map.class)
+                .isEqualTo("$.host", "febit.org")
+                .isNull("$.missing");
+    }
+
+    @Test
+    void as() {
+        root$.asString("$.name")
+                .isNotEmpty()
+                .isEqualTo("febit")
         ;
 
+        root$.asObject("$.age")
+                .isInstanceOf(Integer.class)
+                .isEqualTo(10)
+        ;
+
+        root$.as("$.name", String.class, Assertions::assertThat)
+                .isNotEmpty()
+                .isEqualTo("febit");
+
+        root$.asMap("$.meta")
+                .isNotNull()
+                .hasSize(1)
+                .containsEntry("host", "febit.org")
+        ;
+
+        root$.dive("$.tags")
+                .asList("$")
+                .hasSize(3)
+                .containsExactly("java", "json", "rpc")
+        ;
+    }
+
+    @Test
+    void tags$() {
+        var tags$ = root$.dive("$.tags");
+        tags$.isNotNull()
+                .isInstanceOf(List.class)
+                .isEqualTo("$.[0]", "java")
+                .isNull("$.meta.missing")
+        ;
     }
 
 }
